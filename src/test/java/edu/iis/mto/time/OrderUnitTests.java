a2390132b47f0985048f5cbb662a40b9ccb8b62b
Adam Rezner <195047@edu.p.lodz.pl>
2017-04-11 13:45:05 +0200

    OrderUnitTests class created

---
+++ src/test/java/edu/iis/mto/time/OrderUnitTests.java
@@ -0,0 +1,14 @@
+package edu.iis.mto.time;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OrderUnitTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
