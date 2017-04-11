c0f01657d7483deb5e3a76f32cf0bd1a50ac4c93
Adam Rezner <195047@edu.p.lodz.pl>
2017-04-11 13:46:44 +0200

    OrderUnitTests: confirming default time test

--- src/test/java/edu/iis/mto/time/OrderUnitTests.java
+++ src/test/java/edu/iis/mto/time/OrderUnitTests.java
@@ -4,11 +4,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import edu.iis.mto.fakeTime.DefaultTimeSrc;
+import edu.iis.mto.time.Order;
+import edu.iis.mto.time.OrderExpiredException;
+
 public class OrderUnitTests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	 public void confirmDefaultTime() {
+  		Order order = new Order(new DefaultTimeSrc());
+  		order.submit();
+  		order.confirm();
 	}
-
 }
