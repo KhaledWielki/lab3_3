f3d0d8e83d52e078d78473ee8220112e2128369b
Adam Rezner <195047@edu.p.lodz.pl>
2017-04-11 13:39:20 +0200

    DefaultTimeSrc created

---
+++ src/main/java/edu/iis/mto/fakeTime/DefaultTimeSrc.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package edu.iis.mto.fakeTime;
+
+
+public class DefaultTimeSrc implements TimeSource {
+
+	@Override
+	public long currentTimeMillis() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
